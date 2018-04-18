2b3b849eae18c77747114f7c4d338c4f5d57b255
Rafal Nowakowski <rafal1nowakowski@gmail.com>
2018-04-18 22:49:49 +0200

    validations - fix methods names in controller - change @transactional to the one from spring - validations for creating bid - fix auction model

--- src/main/java/com/nowakowski/auctionservice/auction/AuctionRepository.java
+++ src/main/java/com/nowakowski/auctionservice/auction/AuctionRepository.java
@@ -7,10 +7,9 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
-
-interface AuctionRepository extends CrudRepository<Auction, Long> {
+public interface AuctionRepository extends CrudRepository<Auction, Long> {
 
     @Transactional
     @Modifying
--- src/main/java/com/nowakowski/auctionservice/bid/BidRepository.java
+++ src/main/java/com/nowakowski/auctionservice/bid/BidRepository.java
@@ -1,6 +1,7 @@
 package com.nowakowski.auctionservice.bid;
 
 import com.nowakowski.auctionservice.model.Bid;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,4 +10,8 @@ interface BidRepository extends CrudRepository<Bid, Long> {
 
     @Query("from Bid  b where b.auction.auctionId=:auctionId")
     Iterable<Bid> findBy(@Param("auctionId") Long auctionId);
+
+    @Modifying
+    @Query("UPDATE Auction a SET a.currentPrice = :newPrice WHERE a.auctionId = :id")
+    void updateCurrentPrice(@Param("id") Long id, @Param("newPrice") Long newPrice);
 }
